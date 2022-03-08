f655dc2f79875f915361f870744c8ab7ff9618e6
Trần Duy Việt <duyviet2841998@gmail.com>
2022-03-08 10:24:22 +0700

    rebase gallery items

--- src/components/image-gallery/ImageGallery.vue
+++ src/components/image-gallery/ImageGallery.vue
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, PropType } from 'vue'
 import { GalleryItem } from '../../typing/gallery'
 import InputFixer from '../form/fixer/InputFixer.vue'
 import MultiFixer from '../form/fixer/MultiFixer.vue'
@@ -16,185 +16,37 @@ export default defineComponent({
     MultiFixer,
     Accordion
   },
-  data(): {
-    items: GalleryItem[]
-    columns: number
-    gap: number
-    cls: string
-    newItem: GalleryItem
-    fixingItem: GalleryItem
-    showModal: boolean
-    isAdd: boolean
-    lastIndex?: number
-    showLightBox: boolean
-  } {
-    return {
-      items: [
-        {
-          src: 'https://picsum.photos/200/300',
-          title: 'Just add your desired ',
-          thumbnail: 'https://picsum.photos/200/300',
-          alt: 'Just add your desired'
-        },
-        {
-          src: 'https://picsum.photos/200/300',
-          title: 'Just add your desired ',
-          thumbnail: 'https://picsum.photos/200/300',
-          alt: 'Just add your desired'
-        },
-        {
-          src: 'https://picsum.photos/200/300',
-          title: 'Just add your desired ',
-          thumbnail: 'https://picsum.photos/200/300'
-        },
-        {
-          src: 'https://picsum.photos/200/300',
-          title: 'Just add your desired ',
-          thumbnail: 'https://picsum.photos/200/300'
-        },
-        {
-          src: 'https://picsum.photos/200/300',
-          title: 'Just add your desired ',
-          thumbnail: 'https://picsum.photos/200/300'
-        },
-        {
-          src: 'https://picsum.photos/200/300',
-          title: 'Just add your desired ',
-          thumbnail: 'https://picsum.photos/200/300'
-        },
-        {
-          src: 'https://picsum.photos/200/300',
-          title: 'Just add your desired ',
-          thumbnail: 'https://picsum.photos/200/300'
-        }
-      ],
-      columns: 4,
-      gap: 1,
-      cls: 'grid grid-cols-4 gap-1',
-      newItem: { src: '', title: '', thumbnail: '' },
-      fixingItem: { src: '', title: '', thumbnail: '', index: 0 },
-      showModal: false,
-      isAdd: false,
-      lastIndex: 0,
-      showLightBox: false
-    }
+  props: {
+    items: Array as PropType<GalleryItem[]>,
+    columns: Number,
+    gap: Number,
+    cls: String,
+    showModal: Boolean,
+    isAdd: Boolean,
+    lastIndex: Number,
+    showLightBox: Boolean,
+    newItem: Object as PropType<GalleryItem>,
+    fixingItem: Object as PropType<GalleryItem>
   },
-  methods: {
-    onChangeParams(columns: number, gap: number) {
-      this.$data.cls = `grid grid-cols-${columns} gap-${gap}`
-    },
-    onChangAddState() {
-      this.$data.isAdd = !this.$data.isAdd
-    },
-    onAddNewItem(event: Event) {
-      this.$data.items.push(this.$data.newItem)
-    },
-    onShowLightBox(item: {
-      src: string
-      title: string
-      thumbnail: string
-      index: number
-    }) {
-      this.$data.showLightBox = true
-      this.$data.fixingItem = item
-    }
-  },
-  computed: {
-    getInfo(): { name: string; items: GalleryItem[]; columns: number } {
-      return {
-        name: 'gallery',
-        items: this.$data.items,
-        columns: this.$data.columns
-      }
-    }
-  },
-  onMounted() {},
-  whenDepsChange(update: any) {
-    console.log(update)
-  }
+  methods: {}
 })
 </script>
 
 <template>
   <main class="grid grid-cols-8 m-2">
-    <div class="col-span-3 p-4">
-      <Accordion title="Gallery">
-        <div class="ml-2">
-          <div>
-            <label> Columns </label>
-            <input
-              id="gap"
-              type="number"
-              v-model="columns"
-              @change="onChangeParams(columns, gap)"
-              class="w-full"
-            />
-          </div>
-          <div>
-            <label> Gap </label>
-            <input
-              type="number"
-              v-model="gap"
-              class="w-full"
-              @change="onChangeParams(columns, gap)"
-            />
-          </div>
-
-          <div class="m-2" v-show="isAdd">
-            <form @submit.prevent="onAddNewItem">
-              <input v-model="newItem.src" type="string" placeholder="src" />
-              <input
-                v-model="newItem.title"
-                type="string"
-                placeholder="title"
-              />
-              <input
-                v-model="newItem.thumbnail"
-                type="string"
-                placeholder="thumbnail"
-              />
-              <button id="show-modal c " @click="showModal = true">Fix</button>
-            </form>
-          </div>
-        </div>
-        <!--items-->
-        <div>
-          <Accordion title="Items">
-            <ul class="ml-4">
-              <li v-for="(item, i) in items" :key="i">
-                <Accordion :title="'img' + i">
-                  <div class="ml-2">
-                    <input class="ml-2" v-model="item.src" />
-                    <input class="ml-2" v-model="item.thumbnail" />
-                    <input class="ml-2" v-model="item.title" />
-                  </div>
-                </Accordion>
-              </li>
-            </ul>
-            <button
-              class="btn border btn-primary m-1 p-1"
-              @click="onChangAddState"
-            >
-              + Add more
-            </button>
-          </Accordion>
-        </div>
-      </Accordion>
-    </div>
-    <div class="col-span-5">
       <LightBox
         :show="showLightBox"
         @close="showLightBox = false"
-        :src="fixingItem.src"
+        v-bind:src="fixingItem.src"
       />
       <!--modal-->
       <modal
         :show="showModal"
-        @close=";(showModal = false), (lastIndex = fixingItem?.index)"
-        :title="fixingItem.title"
-        :src="fixingItem.src"
-        :thumbnail="fixingItem.thumbnail"
-        :index="fixingItem.index"
+        @close="(showModal = false), (lastIndex = fixingItem?.index)"
+        v-bind:title="fixingItem?.title!"
+        v-bind:src="fixingItem.src"
+        v-bind:thumbnail="fixingItem.thumbnail"
+        v-bind:index="fixingItem.index"
       >
         <template>
           <h3>Fix item {{ fixingItem.index }}</h3>
@@ -205,14 +57,13 @@ export default defineComponent({
       <div v-bind:class="cls">
         <div v-for="(item, index) in items" :key="index">
           <img
-            :src="item.thumbnail"
+            v-bind="item.thumbnail"
             :aria-label="item.thumbnail"
             class="ri w-full"
-            @click="onShowLightBox({ ...item, index })"
+            @click="$emit('show-light-box', { ...item, index })"
           />
         </div>
       </div>
-    </div>
   </main>
 </template>
 
--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -1,14 +1,156 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import ImageGallery from '../components/image-gallery/ImageGallery.vue'
+import { GalleryItem } from '../typing/gallery'
+import Accordion from '../components/ui/Accordion.vue'
 
 export default defineComponent({
   name: 'gallery',
   components: {
-    ImageGallery
-  }
+    ImageGallery,
+    Accordion
+  },
+  data(): {
+    items: GalleryItem[]
+    columns: number
+    gap: number
+    cls: string
+    newItem: GalleryItem
+    fixingItem: GalleryItem
+    showModal: boolean
+    isAdd: boolean
+    lastIndex?: number
+    showLightBox: boolean
+  } {
+    return {
+      items: [
+        {
+          src: 'https://picsum.photos/200/300',
+          title: 'Just add your desired ',
+          thumbnail: 'https://picsum.photos/200/300',
+          alt: 'Just add your desired'
+        },
+        {
+          src: 'https://picsum.photos/200/300',
+          title: 'Just add your desired ',
+          thumbnail: 'https://picsum.photos/200/300',
+          alt: 'Just add your desired'
+        },
+        {
+          src: 'https://picsum.photos/200/300',
+          title: 'Just add your desired ',
+          thumbnail: 'https://picsum.photos/200/300'
+        },
+        {
+          src: 'https://picsum.photos/200/300',
+          title: 'Just add your desired ',
+          thumbnail: 'https://picsum.photos/200/300'
+        },
+        {
+          src: 'https://picsum.photos/200/300',
+          title: 'Just add your desired ',
+          thumbnail: 'https://picsum.photos/200/300'
+        },
+        {
+          src: 'https://picsum.photos/200/300',
+          title: 'Just add your desired ',
+          thumbnail: 'https://picsum.photos/200/300'
+        },
+        {
+          src: 'https://picsum.photos/200/300',
+          title: 'Just add your desired ',
+          thumbnail: 'https://picsum.photos/200/300'
+        }
+      ],
+      columns: 4,
+      gap: 1,
+      cls: 'grid grid-cols-4 gap-1',
+      newItem: { src: '', title: '', thumbnail: '' },
+      fixingItem: { src: '', title: '', thumbnail: '', index: 0 },
+      showModal: false,
+      isAdd: false,
+      lastIndex: 0,
+      showLightBox: false
+    }
+  },
+  methods: {
+    onChangeParams(columns: number, gap: number) {
+      this.$data.cls = `grid grid-cols-${columns} gap-${gap}`
+    },
+    onChangAddState() {
+      this.$data.isAdd = !this.$data.isAdd
+    },
+    onAddNewItem(event: Event) {
+      this.$data.items.push(this.$data.newItem)
+    },
+    onShowLightBox(item: {
+      src: string
+      title: string
+      thumbnail: string
+      index: number
+    }) {
+      this.$data.showLightBox = true
+      this.$data.fixingItem = item
+    }
+  },
+  computed: {
+    getInfo(): { name: string; items: GalleryItem[]; columns: number } {
+      return {
+        name: 'gallery',
+        items: this.$data.items,
+        columns: this.$data.columns
+      }
+    }
+  },
+  onMounted() {},
+  whenDepsChange(update: any) {
+    console.log(update)
+  },
+  emit: ['show-light-box']
 })
 </script>
 <template>
-  <ImageGallery />
+  <div class="grid grid-cols-8">
+    <div class="col-span-2">
+      <Accordion title="Gallery">
+        <Accordion title="Columns">
+          <input id="gap" type="number" v-model="columns" class="w-full" />
+        </Accordion>
+        <Accordion title="Gap">
+          <input id="gap" type="number" v-model="columns" class="w-full" />
+        </Accordion>
+        <div>
+          <Accordion title="Items">
+            <ul class="ml-4">
+              <li v-for="(item, i) in items" :key="i">
+                <Accordion :title="'img' + i">
+                  <div class="ml-2">
+                    <input class="ml-2" v-model="item.src" />
+                    <input class="ml-2" v-model="item.thumbnail" />
+                    <input class="ml-2" v-model="item.title" />
+                  </div>
+                </Accordion>
+              </li>
+            </ul>
+            <button
+              class="btn border btn-primary m-1 p-1"
+              @click="onChangAddState"
+            >
+              + Add more
+            </button>
+          </Accordion>
+        </div>
+      </Accordion>
+    </div>
+    <div class="col-span-4">
+      <ImageGallery
+        v-bind:cls="cls"
+        v-bind:columns="columns"
+        v-bind:fixingItem="fixingItem"
+        v-bind:gap="gap"
+        v-bind:newItem="newItem"
+        @show-light-box=""
+      />
+    </div>
+  </div>
 </template>
