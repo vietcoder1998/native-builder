f755359165bf065a7ffcc0a8ca63f95cfc1c94a5
viettd <duyviet2841998@gmail.com>
2022-03-08 09:40:12 +0700

    add layout for beaeelement

--- src/components/beae-element/BeaeElement.vue
+++ src/components/beae-element/BeaeElement.vue
@@ -9,29 +9,30 @@ export default defineComponent({
     Accordion
   },
 
-  data(): {
-    collections: ProductCollection
-    col: number
-    gap: number
-  } {
-    return {
-      col: 4,
-      gap: 2,
-      collections: Object.assign(
-        {} as PropType<ProductCollection>,
-        new Array(20).fill(
-          { 
-            thumbnail: 'https://picsum.photos/200/300',
-            productTitle: 'Sp',
-            price: 1.0,
-            addToCart: false
-          },
-          0,
-          20
-        )
-      )
-    }
+  props: {
+    collections: Object,
+    col: Number,
+    gap: Number,
   },
+  // {
+  //   return {
+  //     col: 4,
+  //     gap: 2,
+  //     collections: Object.assign(
+  //       {} as PropType<ProductCollection>,
+  //       new Array(20).fill(
+  //         { 
+  //           thumbnail: 'https://picsum.photos/200/300',
+  //           productTitle: 'Sp',
+  //           price: 1.0,
+  //           addToCart: false
+  //         },
+  //         0,
+  //         20
+  //       )
+  //     )
+  //   }
+  // },
   methods: {
     forceRender() {
       console.log('force update beae element')
@@ -45,10 +46,9 @@ export default defineComponent({
       deep: true
     }
   },
-
   computed: {
     itemCls(): string {
-      return `grid grid-cols-${this.$data.col} gap-${this.$data.gap}`
+      return `grid grid-cols-${this.$props.col} gap-${this.$props.gap}`
     }
   }
 })
@@ -56,29 +56,7 @@ export default defineComponent({
 <template>
   <div class="grid grid-cols-8">
     <div class="col-span-2">
-      <Accordion title="Columns">
-        <input type="number" v-model="col" class="w-full" id="cols-quantity" />
-      </Accordion>
-      <Accordion title="Gaps">
-        <input type="number" v-model="col" class="w-full" id="cols-quantity" />
-      </Accordion>
-      <Accordion :title="'Collection'">
-        <Accordion
-          v-for="(collection, id) in collections"
-          :title="String(id)"
-          v-bind:key="String(id)"
-        >
-          <div
-            class="col-span-1"
-            v-for="(value, key) in collection"
-            :value="value"
-            :key="key"
-          >
-            <label> {{ key }} </label>
-            <input class="w-full" v-model="collections[id][key]" />
-          </div>
-        </Accordion>
-      </Accordion>
+     
     </div>
     <div class="col-span-6 p-2">
       <div v-bind:class="itemCls">
--- src/views/BeaeCollection.vue
+++ src/views/BeaeCollection.vue
@@ -1,11 +1,33 @@
 <script lang="ts">
-import { defineComponent } from 'vue';
-import BeaeElement from '../components/beae-element/BeaeElement.vue';
+import { defineComponent, PropType } from 'vue'
+import BeaeElement from '../components/beae-element/BeaeElement.vue'
+import { ProductCollection } from '../typing/beae.element'
+import Accordion from '../components/ui/Accordion.vue'
 
 export default defineComponent({
   name: 'beae-collection',
   components: {
-    BeaeElement
+    BeaeElement,
+    Accordion
+  },
+  data() {
+    return {
+      col: 4,
+      gap: 1,
+      collections: Object.assign(
+        {} as PropType<ProductCollection>,
+        new Array(20).fill(
+          {
+            thumbnail: 'https://picsum.photos/200/300',
+            productTitle: 'Sp',
+            price: 1.0,
+            addToCart: false
+          },
+          0,
+          20
+        )
+      )
+    }
   },
   mouted() {
     console.log('mounting')
@@ -13,7 +35,40 @@ export default defineComponent({
 })
 </script>
 <template>
-  <BeaeElement ></BeaeElement>
+  <div class="grid grid-cols-8">
+    <div class="col-span-2">
+      <Accordion title="Columns">
+        <input type="number" v-model="col" class="w-full" id="cols-quantity" />
+      </Accordion>
+      <Accordion title="Gaps">
+        <input type="number" v-model="col" class="w-full" id="cols-quantity" />
+      </Accordion>
+      <Accordion :title="'Collection'">
+        <Accordion
+          v-for="(collection, id) in collections"
+          :title="String(id)"
+          v-bind:key="String(id)"
+        >
+          <div
+            class="col-span-1"
+            v-for="(value, key) in collection"
+            :value="value"
+            :key="key"
+          >
+            <label> {{ key }} </label>
+            <input class="w-full" v-model="collections[id][key]" />
+          </div>
+        </Accordion>
+      </Accordion>
+    </div>
+    <div class="col-span-4">
+      <BeaeElement
+        v-bind:collections="collections"
+        v-bind:col="col"
+        v-bind:gap="gap"
+      ></BeaeElement>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
